3e15abde9acbf71e2cd3fecb5173260eabe31972
R3KT19 <batararaja19@gmail.com>
2023-02-23 10:52:12 +0700

    first commit

---
+++ movie/core/data/source/local/entitiy/MovieEntity.kt
@@ -0,0 +1,14 @@
+package com.batararajadamanik.movie.core.data.source.local.entitiy
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movie")
+data class MovieEntity (
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = )
+    var
+)
